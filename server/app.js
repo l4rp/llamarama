4204023f8bb4428256338666915b2e300f16b8db
Chris Newton <chrisnewtn@gmail.com>
2012-04-27 12:59:17 +0100

    Point static directory to /../assets

--- server/app.js
+++ server/app.js
@@ -11,12 +11,10 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/../assets'));
 });
 
 app.configure('development', function(){
