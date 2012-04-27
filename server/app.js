2610b1ac3eed8e217de258c79902d0c0ae17a5fa
Chris Newton <chrisnewtn@gmail.com>
2012-04-27 13:13:30 +0100

    Add socket.io. Add view middleware

--- server/app.js
+++ server/app.js
@@ -4,13 +4,15 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
+  , routes = require('./routes')
+  , app = module.exports = express.createServer()
+  , sio = require('socket.io').listen(app);
 
 // Configuration
 
 app.configure(function(){
+	app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -31,4 +33,17 @@ app.get('/', routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+// Socket stuff
+
+sio.sockets.on('connection', function (socket) {
+	
+	//socket.emit('msg', {'test':'Hello, world!'});
+	
+  // When a message is recieved
+  /*socket.on('test', function (data) {
+    socket.emit('msg', data);
+    socket.broadcast.emit('msg', data);
+  });*/
 });
