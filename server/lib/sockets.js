60185e138920d20fb0eab90af2e77809f8a03798
Aaron Acerboni <a.acerboni@gmail.com>
2012-04-27 15:13:30 +0100

    Encapsulate socket connection handler in function

--- server/lib/sockets.js
+++ server/lib/sockets.js
@@ -1,12 +1,12 @@
-var sockets = require('socket.io');
+function initListeners(io) {
+    io.sockets.on('connection', function (socket) {
+        console.log('socket.js: A socket connection has connected.');
 
-sockets.io.on('connection', function (socket) {
-    console.log('socket.js: A socket connection has connected.');
+        socket.on('test', function (data) {
+           console.log('recieved:', data);
+        });
 
-    socket.on('test', function (data) {
-       console.log('recieved:', data);
     });
+}
 
-});
-
-export.sockets = sockets;
+exports.initListeners = initListeners;
